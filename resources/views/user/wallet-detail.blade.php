ad4d793dc7475e2fa25fabc0768d4049edb64e12
ucsoftng <uchesco.eze1.ua@gmail.com>
2025-05-28 21:54:50 +0100

    commit

--- resources/views/user/wallet-detail.blade.php
+++ resources/views/user/wallet-detail.blade.php
@@ -15,7 +15,27 @@
         </div>
 
         <div class="card p-2">
-            <div id="container"></div>
+{{--            <div id="container"></div>--}}
+            <!-- TradingView Widget BEGIN -->
+            <div class="tradingview-widget-container" style="height:100%;width:100%">
+                <div class="tradingview-widget-container__widget" style="height:calc(100% - 32px);width:100%"></div>
+                <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+                <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
+                    {
+                        "width": "100%",
+                        "height": 450,
+                        "symbol": "BINANCE:{{strtoupper($wallet->short)}}USDT",
+                        "interval": "D",
+                        "timezone": "Etc/UTC",
+                        "theme": "dark",
+                        "style": "1",
+                        "locale": "en",
+                        "allow_symbol_change": true,
+                        "support_host": "https://www.tradingview.com"
+                    }
+                </script>
+            </div>
+            <!-- TradingView Widget END -->
         </div>
         <hr>
         <div class="card card-style">
